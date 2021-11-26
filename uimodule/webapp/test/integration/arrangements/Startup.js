b3fe0c7b8e02fa4edde32d10850cc9c962f7cd30
Xper0 <zas123@inbox.ru>
2021-11-26 17:03:10 +0300

    Initialize repository with easy-ui5

---
+++ uimodule/webapp/test/integration/arrangements/Startup.js
@@ -0,0 +1,19 @@
+sap.ui.define(["sap/ui/test/Opa5"],
+    /**
+     * @param {typeof sap.ui.test.Opa5} Opa5 
+     */
+    function (Opa5) {
+        "use strict";
+
+        return Opa5.extend("com.storeapp.storeapp.test.integration.arrangements.Startup", {
+            iStartMyApp: function () {
+                this.iStartMyUIComponent({
+                    componentConfig: {
+                        name: "com.storeapp.storeapp",
+                        async: true,
+                        manifest: true
+                    }
+                });
+            }
+        });
+    });
